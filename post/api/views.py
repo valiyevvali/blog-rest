ba360e7b23c90cf179851d4651d0ac86b4488d25
Valiyev Vali <valiyevveli@outlook.com>
2021-08-30 15:28:33 +0400

    serializers intro

--- post/api/views.py
+++ post/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import ListAPIView
-
+from post.api.serializers import PostSerializer
 from post.models import Post
 
 
 class PostListApiView(ListAPIView):
     queryset = Post.objects.all()
+    serializer_class = PostSerializer
