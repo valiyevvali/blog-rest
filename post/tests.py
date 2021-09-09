3cde7514c3909c0822513a5fd521b3f589ac3865
Valiyev Vali <valiyevveli@outlook.com>
2021-09-09 15:26:27 +0400

    post delete and update tests added

--- post/tests.py
+++ post/tests.py
@@ -45,3 +45,66 @@ class PostCreateList(APITestCase):
         self.test_add_new_post()
         response = self.client.get(self.url_list)
         self.assertTrue(len(json.loads(response.content)["results"]) == Post.objects.all().count())
+
+
+
+
+class PostUpdateDelete(APITestCase):
+    login_url = reverse("token_obtain_pair")
+
+    def setUp(self):
+        self.username = "veli"
+        self.password = "test1234"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.user2 = User.objects.create_user(username="veli2", password=self.password)
+        self.post = Post.objects.create(title="header", content="test")
+        self.url = reverse("post:update", kwargs={"slug": self.post.slug})
+        self.url_detail = reverse("post:detail", kwargs={"slug": self.post.slug})
+        self.test_jwt_authentication()
+
+    def test_jwt_authentication(self, username="veli", password="test1234"):
+        response = self.client.post(self.login_url, data={"username": username, "password": password})
+        self.assertEqual(200, response.status_code)
+        self.assertTrue("access" in json.loads(response.content))
+        self.token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+
+    def test_post_delete(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(204, response.status_code)
+
+
+    def test_post_delete_different_user(self):
+        self.test_jwt_authentication("veli2")
+        response = self.client.delete(self.url)
+        self.assertEqual(403, response.status_code)
+
+
+    def test_post_update(self):
+        data = {
+            'content':  'test',
+            'title': 'test header'
+        }
+
+        response = self.client.put(self.url, data)
+        self.assertEqual(200, response.status_code)
+        self.assertTrue(Post.objects.get(id=self.post.id).content == data["content"])
+
+
+
+    def test_post_update_different_user(self):
+        self.test_jwt_authentication("veli2")
+        data = {
+            'content':  'test',
+            'title': 'test header'
+        }
+
+        response = self.client.put(self.url, data)
+        self.assertEqual(403, response.status_code)
+
+
+
+    def test_unauthorization(self):
+        self.client.credentials()
+        response = self.client.get(self.url)
+        self.assertEqual(401, response.status_code)
